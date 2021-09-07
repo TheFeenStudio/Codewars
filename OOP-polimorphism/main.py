bae1a0a5de0b1a6dc3b203b82620b04015425d4f
TheFeenStudio <mbannakov14@yahoo.com>
2021-09-07 21:59:25 +0300

    'almost_nothing_changed'

--- OOP-polimorphism/main.py
+++ OOP-polimorphism/main.py
@@ -11,17 +11,16 @@ class NewSFTP(SFTPClient):
             print('sorted list')
             return sorted(lst, key=attrgetter('st_mtime'))
         else:
+            print('List has not been sorted, switch your flag')
             return lst
-
 
     def newest(self, path="."):  # метод возвращающий имя самого свежего файла (по дате создания)
         lst = self.listdir_attr(path)
         print(lst[-1].filename)
 
-
     def get(self, remotepath, localpath, callback=None):
-        get = super().get(remotepath, localpath, callback=None)
-        print(get)
+        path = Path(localpath)
+        get = super().get(remotepath, path, callback=None)
 
 
 # INFO
@@ -29,10 +28,11 @@ host = "134.0.115.28"
 username = "root"
 password = "ap4Ainee4uma"
 
-# Connect
+
+# Connection
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 client.connect(hostname=host, username=username, password=password, look_for_keys=False, allow_agent=False)
-sftp = SFTPClient.from_transport(client.get_transport())
-# list = sftp.listdir_attr(flag=True)
-newest = sftp.get('/Users/maxim/desktop',)
+sftp = NewSFTP.from_transport(client.get_transport())
+
+newest = sftp.get('test.txt', '5555.txt')
