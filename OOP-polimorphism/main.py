0f1c11fe6bd20e98245973294805a7033a6a35e3
AVVasiliev <haron2012@mail.ru>
2021-09-08 16:36:35 +0300

    Комментарии

--- OOP-polimorphism/main.py
+++ OOP-polimorphism/main.py
@@ -12,10 +12,12 @@ class NewSFTP(SFTPClient):
             return lst
 
     def newest(self, path="."):  # метод возвращающий имя самого свежего файла (по дате создания)
+        # можно же было просто свой же метод вызвать с флагом
+        # return self.listdir_attr(path, True)[-1].filename  # реализацию незачем дублировать
         lst = super().listdir_attr(path)
         lst = sorted(lst, key=attrgetter('st_mtime'))
         return lst[-1].filename
 
     def _adjust_cwd(self, path):
-        p = Path(path)
+        p = Path(path) # вот зачем это здесь?) я честно не пойму для чего нужно
         return super()._adjust_cwd(str(p))  # возможность передавать объект Path из pathlib в качестве пути в методах get, put, remove
