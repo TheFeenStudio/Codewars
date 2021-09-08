fef9728ef67abeb41072767a5ebd83f59b96eb71
TheFeenStudio <mbannakov14@yahoo.com>
2021-09-08 19:36:13 +0300

    'end'

--- OOP-polimorphism/main.py
+++ OOP-polimorphism/main.py
@@ -1,7 +1,7 @@
 from paramiko import SFTPClient
 from operator import attrgetter
 from pathlib import Path
-
+import paramiko
 
 class NewSFTP(SFTPClient):
     def listdir_attr(self, path=".", sort=False):  # модифицировать метод listdir
@@ -12,10 +12,18 @@ class NewSFTP(SFTPClient):
             return lst
 
     def newest(self, path="."):  # метод возвращающий имя самого свежего файла (по дате создания)
-        # можно же было просто свой же метод вызвать с флагом
-        # return self.listdir_attr(path, True)[-1].filename  # реализацию незачем дублировать
         lst = self.listdir_attr(path, sort=True)
         return lst[-1].filename
 
     def _adjust_cwd(self, path):
         return super()._adjust_cwd(str(path))  # возможность передавать объект Path из pathlib в качестве пути в методах get, put, remove
+
+#INFO
+host = ''
+username = ''
+password = ''
+
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect(hostname=host, username=username, password=password, look_for_keys=False, allow_agent=False)
+sftp = NewSFTP.from_transport(client.get_transport())
