9d12b3fc9a4032633868a26cc23c7cb322b3c81c
AVVasiliev <haron2012@mail.ru>
2021-09-08 15:33:17 +0300

    Комментарии к задаче ООП

--- OOP-polimorphism/main.py
+++ OOP-polimorphism/main.py
@@ -4,12 +4,17 @@ from operator import attrgetter
 from pathlib import Path
 
 from paramiko.py3compat import b
+# лишние импорты лучше подчищать
 
 
 class NewSFTP(SFTPClient):
     def listdir_attr(self, path=".", flag=True, ):  # модифицировать метод listdir
+        # параметр flag выглядит неочевидно, раз он отвечает за сортировку его бы и назвать соответственно
+        # модифицируя что-то для совместимости все добавочные параметры (наш flag) при значении по умолчанию
+        # не должны ничего менять, а так вы сразу подменяете результат функции при параметрах по умолчанию
         lst = super().listdir_attr(path)
         if flag:
+            # pring излишен, это выглядит как строка для дебага, а наша функция имеет конкертное предназначение
             print('sorted list')
             return sorted(lst, key=attrgetter('st_mtime'))
         else:
@@ -18,18 +23,25 @@ class NewSFTP(SFTPClient):
 
     def newest(self, path="."):  # метод возвращающий имя самого свежего файла (по дате создания)
         lst = self.listdir_attr(path)
+        # вместе с перевым методом, решение выглядит не самым очевидныи образом - явное лучше неявного
         return lst[-1].filename
 
     def _adjust_cwd(self, path):
+        # Здесь накручено что-то лишнее - почему конвертация в bytes? Зачем конвертация в Path? - по заданию мы итак передаем Path
+        # получается Path(Path(..)) что не очень логично
         return super()._adjust_cwd(bytes(
             Path(path)))  # возможность передавать объект Path из pathlib в качестве пути в методах get, put, remove
 
 
 # INFO
+# проект конечно тестовый и в приватном репозитории, но персональные данные лучше не публиковать - они не имеют отношения к нашему классу,
+# и нужны чисто для тестирования лично вам
 host = "134.0.115.28"
 username = "root"
 password = "ap4Ainee4uma"
 
+# по задаче просилось делать подключение? требовалось поправить класс
+# а сейчас если бы я его импортировал в свой модуль у меня выполнится лишний ненужный код
 # Connection
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
