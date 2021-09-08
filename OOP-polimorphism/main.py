f0fbcaa3efcbf4c27ab1c750f1f8b55f7b9078d6
TheFeenStudio <mbannakov14@yahoo.com>
2021-09-08 14:48:45 +0300

    finished

--- OOP-polimorphism/main.py
+++ OOP-polimorphism/main.py
@@ -21,10 +21,8 @@ class NewSFTP(SFTPClient):
         return lst[-1].filename
 
     def _adjust_cwd(self, path):
-        new_path = Path(path)
-        super()._adjust_cwd(b(new_path))
-
-
+        return super()._adjust_cwd(bytes(
+            Path(path)))  # возможность передавать объект Path из pathlib в качестве пути в методах get, put, remove
 
 
 # INFO
@@ -32,11 +30,8 @@ host = "134.0.115.28"
 username = "root"
 password = "ap4Ainee4uma"
 
-
 # Connection
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 client.connect(hostname=host, username=username, password=password, look_for_keys=False, allow_agent=False)
 sftp = NewSFTP.from_transport(client.get_transport())
-
-newest = sftp._adjust_cwd('test.txt')
