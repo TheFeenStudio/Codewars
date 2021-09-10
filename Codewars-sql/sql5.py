84783b0fd554a678dda349b19a27153afea00218
TheFeenStudio <mbannakov14@yahoo.com>
2021-09-10 21:28:51 +0300

    'sql5'

---
+++ Codewars-sql/sql5.py
@@ -0,0 +1,6 @@
+# You will need to create SELECT statement in conjunction with LIKE.
+#
+# Please list people which have first_name with at least 6 character long
+
+
+# select first_name, last_name from names WHERE first_name LIKE '______%'
