8dbbc1392155d2ace30a0b8242124c779e47f664
TheFeenStudio <mbannakov14@yahoo.com>
2021-09-18 20:12:08 +0300

    123

--- celery_project/app/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import MailBox
-
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        app_label = 'app'
-        model = MailBox
-        fields = '__all__'
