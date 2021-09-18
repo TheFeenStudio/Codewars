dc23182fb9e692a631180658be26868108c08ff8
TheFeenStudio <mbannakov14@yahoo.com>
2021-09-18 20:07:12 +0300

    123

--- celery_project/celery_project/settings.py
+++ celery_project/celery_project/settings.py
@@ -94,9 +94,10 @@ AUTH_PASSWORD_VALIDATORS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'mbannakov13@gmail.com'
-EMAIL_HOST_PASSWORD = 'pisikakin1'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
 
 REDIS_HOST = '0.0.0.0'
 REDIS_PORT = '6379'
